358a3ce2ee0ff9d8bbc9a469346b8b2ac357c35f
Borui Wang <wangbd@bc.edu>
2016-08-25 12:27:07 -0500

    Initializing Seam Carver Class

---
+++ src/partTwo_Assignment_2/SeamCarver.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package partTwo_Assignment_2;
+
+import edu.princeton.cs.algs4.Picture;
+
+/**
+ *
+ * @author Borui Wang
+ */
+public class SeamCarver {
+   public SeamCarver(Picture picture)                // create a seam carver object based on the given picture
+   {
+       
+   }
+   public Picture picture()                          // current picture
+   {
+       return null;
+   }
+   public     int width()                            // width of current picture
+   {
+       return -1;
+   }
+   public     int height()                           // height of current picture
+   {
+       return -1;
+   }
+   public  double energy(int x, int y)               // energy of pixel at column x and row y
+   {
+       return -1;
+   }
+   public   int[] findHorizontalSeam()               // sequence of indices for horizontal seam
+   {
+       return null;
+   }
+   public   int[] findVerticalSeam()                 // sequence of indices for vertical seam
+   {
+       return null;
+   }
+   public    void removeHorizontalSeam(int[] seam)   // remove horizontal seam from current picture
+   {
+       
+   }
+   public    void removeVerticalSeam(int[] seam)     // remove vertical seam from current picture
+   {
+       
+   }
+}
