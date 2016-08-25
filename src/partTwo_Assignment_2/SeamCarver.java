bfc7544330b7b00107a31eae7724a80364d12915
Borui Wang <wangbd@bc.edu>
2016-08-25 12:45:18 -0500

    Added Exception Handling in code

--- src/partTwo_Assignment_2/SeamCarver.java
+++ src/partTwo_Assignment_2/SeamCarver.java
@@ -12,9 +12,13 @@ import edu.princeton.cs.algs4.Picture;
  * @author Borui Wang
  */
 public class SeamCarver {
+    private int W;
+    private int H;
    public SeamCarver(Picture picture)                // create a seam carver object based on the given picture
    {
-       
+       if(picture == null) throw new java.lang.NullPointerException("picture is null");
+       H = picture.height();
+       W = picture.width();
    }
    public Picture picture()                          // current picture
    {
@@ -30,6 +34,8 @@ public class SeamCarver {
    }
    public  double energy(int x, int y)               // energy of pixel at column x and row y
    {
+       if(x < 0 || x > W - 1) throw new java.lang.IndexOutOfBoundsException("x is out of range in energy()");
+       if(y < 0 || y > H - 1) throw new java.lang.IndexOutOfBoundsException("y is out of range in energy()");
        return -1;
    }
    public   int[] findHorizontalSeam()               // sequence of indices for horizontal seam
@@ -42,10 +48,12 @@ public class SeamCarver {
    }
    public    void removeHorizontalSeam(int[] seam)   // remove horizontal seam from current picture
    {
-       
+       if(seam == null) throw new java.lang.NullPointerException("seam is null");
+       if(H <= 1) throw new java.lang.IllegalArgumentException("height of the picture is too small");
    }
    public    void removeVerticalSeam(int[] seam)     // remove vertical seam from current picture
    {
-       
+       if(seam == null) throw new java.lang.NullPointerException("seam is null");
+        if(W <= 1) throw new java.lang.IllegalArgumentException("width of the picture is too small");
    }
 }
